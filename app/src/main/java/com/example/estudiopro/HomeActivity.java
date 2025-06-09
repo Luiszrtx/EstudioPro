8a3930f83f0f8cfe13da40b850835cfec2c4ea2c
Luis Rojas <luisrojas@MacBook-Air-de-Luis.local>
2025-06-09 17:28:39 -0600

    Hola

--- app/src/main/java/com/example/estudiopro/HomeActivity.java
+++ app/src/main/java/com/example/estudiopro/HomeActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//hola
 public class HomeActivity extends AppCompatActivity {
 
     @Override
